546ff9fa117d4bd48eb4543f9d75c898c725103a
Jesse <le2op7y3vc859@foxmail.com>
2019-05-30 14:39:15 +0800

    add position of pop☕

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,10 @@
-import React, { useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useRef } from "react";
 import styles from "./styles.module.css";
 
-const Modal = ({
-  children,
-  show,
-  layerStyle,
-  enableLayerClose,
-  makeCloseItself
-}) => {
+const Modal = ({ children, show, layerStyle, pos, makeCloseItself }) => {
+  const popEl = useRef(null);
+  const [offset, setOffset] = useState({});
+
   useLayoutEffect(() => {
     // 弹窗弹出, 禁止窗口滚动
     if (show) {
@@ -16,6 +13,37 @@ const Modal = ({
     return () => {
       document.body.style.overflow = "visible";
     };
+  }, [show]);
+
+  const adjustPos = ({ offset, pos }) => {
+    const { width, height } = offset;
+
+    let { vertical, horizontal } = pos;
+
+    horizontal = horizontal === "center" ? "50%" : `${horizontal}px`;
+    vertical = vertical === "center" ? "50%" : `${vertical}px`;
+
+    const marginLeft =
+      horizontal === "center" ? { marginLeft: `-${width / 2}px` } : {};
+    const marginTop =
+      vertical === "center" ? { marginTop: `-${height / 2}px` } : {};
+
+    return {
+      left: horizontal,
+      top: vertical,
+      ...marginLeft,
+      ...marginTop
+    };
+  };
+
+  useLayoutEffect(() => {
+    if (popEl && show) {
+      const width = popEl.current.offsetWidth;
+      const height = popEl.current.offsetHeight;
+      const offset = adjustPos({ offset: { width, height }, pos });
+      setOffset(offset);
+    }
+    return () => {};
   }, [show]);
 
   const wrapClass = show ? `${styles.wrap} ${styles.show}` : styles.wrap;
@@ -27,7 +55,9 @@ const Modal = ({
         style={layerStyle}
         onClick={makeCloseItself}
       />
-      {children}
+      <div className={styles["pop-wrap"]} ref={popEl} style={offset}>
+        {children}
+      </div>
     </div>
   );
 };
@@ -35,6 +65,10 @@ const Modal = ({
 Modal.defaultProps = {
   show: false,
   makeCloseItself: () => {},
+  pos: {
+    vertical: "center",
+    horizontal: "center"
+  },
   layerStyle: {}
 };
 
